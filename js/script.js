c0627afdee50aabe90e4dafb640f98cd27ef4515
Carlos <contato@carlosmsdesign.com.br>
2022-01-05 16:47:32 -0600

    adequação do css e finalização do js para funcionalidade

--- js/script.js
+++ js/script.js
@@ -4,13 +4,19 @@ const PONTEIROSEGUNDO = document.querySelector("#second");
 
 function rotacaorelogio(){
 
-    let posicaoHora = 10;
-    let posicaoMin = 45;
-    let posicaoSeg = 200;
+    let horaAtual = new Date();
+
+    let hora = horaAtual.getHours();
+    let min = horaAtual.getMinutes();
+    let seg = horaAtual.getSeconds();
+
+    let posicaoHora = (hora*360/12)+(min*(360/60)/12);
+    let posicaoMin = (min*360/60)+(seg*(360/60)/60);
+    let posicaoSeg = seg*360/60;
 
     PONTEIROHORA.style.transform = "rotate(" + posicaoHora + "deg)";
     PONTEIROMINUTO.style.transform = "rotate(" + posicaoMin + "deg)";
     PONTEIROSEGUNDO.style.transform = "rotate(" + posicaoSeg + "deg)";
 }
 
-var movimento = rotacaorelogio();
+var movimento = setInterval(rotacaorelogio, 1000);
