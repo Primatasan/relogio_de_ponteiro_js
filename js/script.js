da7f1bb0df0953e707d4777f2ca8502ced2ef081
Carlos Sanmartin <contato@carlosmsdesgin.com.br>
2022-01-04 21:22:24 -0600

    Adicionada a função para movimentar os ponteiros

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 const PONTEIROHORA = document.querySelector("#hour");
 const PONTEIROMINUTO = document.querySelector("#minute");
 const PONTEIROSEGUNDO = document.querySelector("#second");
+
+function rotacaorelogio(){
+
+    let posicaoHora = 10;
+    let posicaoMin = 45;
+    let posicaoSeg = 200;
+
+    PONTEIROHORA.style.transform = "rotate(" + posicaoHora + "deg)";
+    PONTEIROMINUTO.style.transform = "rotate(" + posicaoMin + "deg)";
+    PONTEIROSEGUNDO.style.transform = "rotate(" + posicaoSeg + "deg)";
+}
+
+var movimento = rotacaorelogio();
